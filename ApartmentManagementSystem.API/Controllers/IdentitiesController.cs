fb2d1885183683f79917d35581783adbe4dae7b0
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-02-11 12:46:21 +0300

    up

--- ApartmentManagementSystem.API/Controllers/IdentitiesController.cs
+++ ApartmentManagementSystem.API/Controllers/IdentitiesController.cs
@@ -8,16 +8,35 @@ namespace ApartmentManagementSystem.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class IdentitiesController(IdentityService identityService) : ControllerBase
+    public class IdentitiesController(IdentityService identityService, TokenService tokenService) : ControllerBase
     {
         [HttpPost]
         public async Task<IActionResult> CreateUser(UserCreateRequestDto request)
         {
             var response = await identityService.CreateUser(request);
             if(response.AnyError)
-            {
                 return BadRequest(response);
-            }
+
+            return Created("", response);
+        }
+
+        public async Task<IActionResult> CreateToken(TokenCreateRequestDto request)
+        {
+            var response = await tokenService.Create(request);
+            
+            if(response.AnyError)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        //Role Assignment
+        public async Task<IActionResult> CreateRole(RoleCreateRequestDto request)
+        {
+            var response = await identityService.CreateRole(request);
+
+            if (response.AnyError)
+                return BadRequest(response);
 
             return Created("", response);
         }
